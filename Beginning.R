10df2c6138f307d8ee5bbb0708f57352486b4d39
picixgiulia <146854720+picixgiulia@users.noreply.github.com>
2023-10-03 17:52:23 +0200

    Update Beginning.R

--- Beginning.R
+++ Beginning.R
@@ -10,3 +10,18 @@ Duccio <- 5 + 3
 # zima*duccio will calculate 48, and we can save it as an object
 final <- zima*duccio
 
+# array
+sophi <- c(10, 20, 30, 50, 70) #microplastic 
+# functions have parenthesis which inside have arguments
+paula <- c(100, 500, 600, 1000, 2000) #people
+plot(paula, sophi) # to find relation between veariables
+# how to chnage label on plot
+plot(paula, sophi, xlab ="number of people", "microplastic")
+# or we can reassign the onject paula to people and spphi to microplastics
+plot(poeple, microplastics)
+plot(poeple, microplastics, psh=19) #to change the type of points, look up number on internet
+plot(poeple, microplastics, psh=19, cex=2) #to change the dimensions of the points
+plot(poeple, microplastics, psh=19, cex=2, col="blue") #to change the colour of the points
+
+# how to install a package 
+install.packages
