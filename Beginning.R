5297cc9896bf5a1c90e5a59ee729c8717f2e04c3
picixgiulia <146854720+picixgiulia@users.noreply.github.com>
2023-10-03 17:04:08 +0200

    Create Beginning.R

---
+++ Beginning.R
@@ -0,0 +1,12 @@
+# Here i can write anything i want. In this way R cannot run this line, it is just a comment to explain what is happening.
+
+# R as a calculator 
+2 + 3 # if you copy paste this in R it will calculate the result
+
+# you can assign a subpart of the code to an object 
+Zima <- 2 + 3
+# now if you write Zima in R and run the code it will appear 5
+Duccio <- 5 + 3 
+# zima*duccio will calculate 48, and we can save it as an object
+final <- zima*duccio
+
