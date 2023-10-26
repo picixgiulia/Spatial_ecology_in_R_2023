44ff9d937f97d319b666094da5ed7d17c6b464ed
picixgiulia <146854720+picixgiulia@users.noreply.github.com>
2023-10-26 16:52:04 +0200

    Create 4.Remote.sensing.data.R

---
+++ 4.Remote.sensing.data.R
@@ -0,0 +1,6 @@
+There is a difference between satellite images (scanner) and photographs.
+The earth is not a sphere, it's a Geoid: surface always perpendicular to the gravity force and is approximating the sea level.
+Since it is not a geometrical shape, the Geoid cannot be used to do measurements, instead we use an ellipsoid.
+We use different ellipsoid based on the region of the world considered, to be the most similar as possible to the real surface.
+Example: NAD27 for USA, ED50 for Italy.
+
