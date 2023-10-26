451296505fe1a6d1c766df0b6ffc2ae88d1f9130
picixgiulia <146854720+picixgiulia@users.noreply.github.com>
2023-10-26 17:29:30 +0200

    Update 4.Remote.sensing.data.R

--- 4.Remote.sensing.data.R
+++ 4.Remote.sensing.data.R
@@ -1,5 +1,5 @@
 There is a difference between satellite images (scanner) and photographs.
-The earth is not a sphere, it's a Geoid: surface always perpendicular to the gravity force and is approximating the sea level.
+The earth is not a sphere, it's a Geoid: the surface is always perpendicular to the gravity force and is approximately as the sea level.
 Since it is not a geometrical shape, the Geoid cannot be used to do measurements, instead we use an ellipsoid.
 We use different ellipsoid based on the region of the world considered, to be the most similar as possible to the real surface.
 Example: NAD27 for USA, ED50 for Italy.
@@ -9,5 +9,15 @@ so when it arrives at higher latutudes the air is dry.
 Latitude: angle between perpendicular line from point P and the equator (not the center of the earth).
 If you start from the same point on the planet and consider different ellipsoids, you will obtain different latitudes. 
 When using latitudes and longitudes we need to specify the Datum (ellipsoid).
-WGS84 matches in most cases the planet, adjusted by satellites. 
+WGS84 matches in most cases the planet, adjusted by satellites.
+
+From ellipsoid to planar system
+Universal Transverse Mercator (UTM) = Gauss projection, frangmentation of the planet system, divide the surface in the meridians (12).
+Every zone is 6 degrees, 60 zones. We start counting zones from the antimeridian of Greenwhich (Hawaii).
+Italy is in 32 and 33 zone.
+
+
+
+
+
 
