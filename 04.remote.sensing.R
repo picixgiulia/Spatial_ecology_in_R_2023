eddb86a6074f0fb0c0125e5a6eba854a04d41589
picixgiulia <146854720+picixgiulia@users.noreply.github.com>
2023-12-27 15:53:21 +0100

    Update 04.remote.sensing.R

--- 04.remote.sensing.R
+++ 04.remote.sensing.R
@@ -96,13 +96,32 @@ plot(b8, col=cl8)
 # b4 red el3, stacksent[[3]]
 # b8 nir el4, stacksent[[4]]
 dev.off()
-im.plotRGB(stacksent, r=3, g=2, b=1)
+im.plotRGB(stacksent, r=3, g=2, b=1) # bands in the visible for humans
 # seems black and white, is actually dark green, this is what we see woth our eyes, 
 # we need other wavelenghts to see it in a meaningful manner (now is very low resolution)
 # the NIR wavelenght
 
+# we can add NIR to obtain a clearer image, but we need to sacrifice a previous band
+# we remove blue and shift by one the bands 
+im.plotRGB(stacksent, r=4, g=3, b=2)
+# epidermis of healthy leaves reflect a lot in the NIR, everything that will appear red is vegetation.
+# NIR is on top of the red component
+# there are two kinds of vegetation, bright red is pastures whereas the darker one is woods
+# we receive additional info compared to the previous one
+im.plotRGB(stacksent, r=3, g=4, b=2) # now the NIR is on top of the red, so everithing green is going to appear fluorescent
+# light green pastures, dark green woods, violet is soil
+im.plotRGB(stacksent, r=3, g=2, b=4) # NIR is on top of blue, vegetation will be blue
+# soil is yellow
 
+# We are not deciding the colors, this are the colors coming out of the reflectance of certain wavelengths
 
+# you want to see the correlation between different bands
+# confronting the values of each pixel in the bands
+# there is a function to do that for you
+# not just geographical data, you can use it for any table
+pairs(stacksent)
+# graph of correlation between each band with relative coefficient
+#the histogram is the frequency of the reflectance
 
 
 
