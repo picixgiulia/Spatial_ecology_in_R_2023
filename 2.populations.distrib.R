e54e727eb6cba3594834054858a7e06e37c62447
picixgiulia <146854720+picixgiulia@users.noreply.github.com>
2023-12-27 11:47:27 +0100

    Update 2.populations.distrib.R

--- 2.populations.distrib.R
+++ 2.populations.distrib.R
@@ -61,3 +61,81 @@ points(species[species$Occurrence == 1,], pch=16)
 
 plot(preds$vegetation, col=cl)
 points(species[species$Occurrence == 1,], pch=16)
+
+# Another way of doing the same
+# Try find relations between presence of frogs and
+# elevation, precipitation, temperature and vegetation
+file <- system.file("external/species.shp", package="sdm")
+
+rana <- vect(file)
+rana$Occurrence
+
+plot(rana)
+
+# Selecting presences
+pres <- rana[rana$Occurrence==1,]
+plot(pres)
+
+# select absence and call them abse
+abse <- rana[rana$Occurrence==0,]
+plot(abse)
+
+# plot presences and absences, one beside the other
+par(mfrow=c(1,2))
+plot(pres)
+plot(abse)
+
+# your new friend in case of graphical nulling:
+dev.off()
+
+# plot pres and abse altogether with two different colours
+plot(pres, col="dark blue")
+points(abse, col="light blue")
+
+# predictors: environmental variables
+# file <- system.file("external/species.shp", package="sdm")
+# rana <- vect(file)
+
+# elevation predictor
+elev <- system.file("external/elevation.asc", package="sdm") 
+elevmap <- rast(elev) # from terra package
+plot(elevmap)
+points(pres, cex=.5)
+
+# temperature predictor
+temp <- system.file("external/temperature.asc", package="sdm") 
+tempmap <- rast(temp) # from terra package
+plot(tempmap)
+points(pres, cex=.5)
+
+# vegetation predictor
+vege <- system.file("external/vegetation.asc", package="sdm") 
+vegemap <- rast(vege) # from terra package
+plot(vegemap)
+points(pres, cex=.5)
+
+# vprecipitation predictor
+prec <- system.file("external/precipitation.asc", package="sdm") 
+precmap <- rast(prec) # from terra package
+plot(precmap)
+points(pres, cex=.5)
+
+# final multiframe
+
+par(mfrow=c(2,2))
+
+# elev
+plot(elevmap)
+points(pres, cex=.5)
+
+# temp
+plot(tempmap)
+points(pres, cex=.5)
+
+# vege
+plot(vegemap)
+points(pres, cex=.5)
+
+# prec
+plot(precmap)
+points(pres, cex=.5)
