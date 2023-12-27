99a5b074b05a85e4dd2368a536457ba8183a374c
picixgiulia <146854720+picixgiulia@users.noreply.github.com>
2023-12-27 15:39:38 +0100

    Update 04.remote.sensing.R

--- 4.remote.sensing.R
+++ 4.remote.sensing.R
@@ -1,15 +1,15 @@
-install.packages("devtools") # install devtools
+# Remote sensing on R
+# This is a script to visualize satellite data
 
+install.packages("devtools") # install devtools
 library(devtools) # use devtools
 # CRA (comprehensive R archive packages)
 # the one we need is not in CRA so we need devtools to obtain packages somewhere else
-
-
-install_github("ducciorocchini/imageRy") # install imagery
+devtools::install_github("ducciorocchini/imageRy") # install imagery
 library(imageRy) # to make a check
 library(terra)
 
-# now we will use a package
+# now we will use a function from the package imageRy (all the functions in this package are preceded by im.)
 im.list()
 
 # importing data
@@ -29,7 +29,6 @@ b4
 b8 <- im.import("sentinel.dolomites.b8.tif") # b2 is the green wavelength
 b8
 
-2/11/23
 # Remote sensing visualization
 
 library(devtools)
@@ -78,6 +77,29 @@ dev.off() #it closes devices
 plot(stacksent, col=cl) # same as before but jut with one function
 
 plot(stacksent[[4]], col=cl) # to plot a single band
+
+# exercise: plot in multiframe the bands with dfferent color ramps
+cl2 <- colorRampPalette(c("lightblue", "blue", "darkblue")) (100)
+cl3 <- colorRampPalette(c("lightgreen", "darkgreen", "black")) (100)
+cl4 <- colorRampPalette(c("pink", "red", "darkred")) (100)
+cl8 <- colorRampPalette(c("yellow", "orange", "darkorange")) (100)
+
+par(mfrow=c(2,2))
+plot(b2, col=cl2)
+plot(b3, col=cl3)
+plot(b4, col=cl4)
+plot(b8, col=cl8)
+
+# stackent: 
+# band2 blue element1, stacksent[[1]]
+# b3 green el2, stacksent[[2]]
+# b4 red el3, stacksent[[3]]
+# b8 nir el4, stacksent[[4]]
+dev.off()
+im.plotRGB(stacksent, r=3, g=2, b=1)
+# seems black and white, is actually dark green, this is what we see woth our eyes, 
+# we need other wavelenghts to see it in a meaningful manner (now is very low resolution)
+# the NIR wavelenght
 
 
 
