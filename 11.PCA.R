cb236c87d777f69525a8d6e02d733022bb5612bb
picixgiulia <146854720+picixgiulia@users.noreply.github.com>
2023-12-28 17:34:11 +0100

    Update 11.PCA.R

--- 11.PCA.R
+++ 11.PCA.R
@@ -16,6 +16,10 @@ pc1 <- sentpc$PC1
 viridisc <- colorRampPalette(viridis(7))(255)
 plot(pc1, col=viridisc)
 
+# standard deviation values
+sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
+sd7 <- focal(nir, matrix(1/49, 3, 3), fun=sd)
+
 # calculating standard deviation ontop of pc1
 pc1sd3 <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
 plot(pc1sd3, col=viridisc)
@@ -25,6 +29,7 @@ plot(pc1sd7, col=viridisc)
 
 par(mfrow=c(2,3))
 im.plotRGB(sent, 2, 1, 3)
+
 # sd from the variability script:
 plot(sd3, col=viridisc)
 plot(sd7, col=viridisc)
